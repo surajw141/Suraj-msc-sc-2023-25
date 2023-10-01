b66396009c91b47d06c23ab9c915e4cc938aa969
surajw141 <surajwaghmare141@gmail.com>
2023-10-01 20:19:25 +0530

    resaved

--- 13 2x2 matrix.c
+++ 13 2x2 matrix.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
   int matrix[2][2];
 
   // Read the matrix from the user
-  for (int i = 0; i < 2; i++) {
-    for (int j = 0; j < 2; j++) {
+  for (int i = 0; i < 2; i++)
+  {
+    for (int j = 0; j < 2; j++)
+    {
       printf("Enter element [%d][%d]: ", i, j);
       scanf("%d", &matrix[i][j]);
     }
@@ -13,8 +16,10 @@ int main() {
 
   // Print the matrix
   printf("The matrix is:\n");
-  for (int i = 0; i < 2; i++) {
-    for (int j = 0; j < 2; j++) {
+  for (int i = 0; i < 2; i++)
+  {
+    for (int j = 0; j < 2; j++)
+    {
       printf("%d ", matrix[i][j]);
     }
     printf("\n");
